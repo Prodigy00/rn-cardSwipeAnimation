74c38e9c455f79a712c38022972b7d2ca671c068
Prodigy00 <simplygiddy@gmail.com>
2020-01-02 10:55:43 +0100

    added panResponder to deck

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { View, Animated, PanResponder } from "react-native";
 
 const Deck = ({ data, renderCard }) => {
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gesture) => {
+          console.log({ ...gesture });
+        },
+        onPanResponderRelease: () => {}
+      }),
+    []
+  );
+
   const renderCards = () => data.map(item => renderCard(item));
 
-  return <View>{renderCards()}</View>;
+  return <View {...panResponder.panHandlers}>{renderCards()}</View>;
 };
 
 export default Deck;
